707c27b0f76431cb4fda9a8aa20e8b08896a7b3a
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-02-23 23:21:25 +0700

    Create delete_Moment.php

---
+++ delete_Moment.php
@@ -0,0 +1,79 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include 'koneksi.php';
+
+$postData = file_get_contents('php://input');
+
+$idt = $_POST['idt']; 
+$agenda = $_POST['agenda']; 
+$tagin = $_POST['tagin'];
+
+if($tagin == "true"){
+
+   $result = mysqli_query($koneksi, "DELETE FROM tag WHERE idt = '$idt' ") or die (mysqli_error());
+
+    if($agenda == "Meeting"){
+
+        $result = mysqli_query($koneksi, "DELETE FROM checkin WHERE idtx = '$idt' ") or die (mysqli_error());
+
+        $result = mysqli_query($koneksi, "DELETE FROM timeline WHERE idt = '$idt' LIMIT 1") or die (mysqli_error());
+    }
+    else{
+
+        $result = mysqli_query($koneksi, "DELETE FROM timeline WHERE idt = '$idt' LIMIT 1") or die (mysqli_error());
+
+    }
+}
+else{
+
+      if($agenda == "Meeting"){
+
+        $result = mysqli_query($koneksi, "DELETE FROM checkin WHERE idtx = '$idt' ") or die (mysqli_error());
+
+        $result = mysqli_query($koneksi, "DELETE FROM timeline WHERE idt = '$idt' LIMIT 1") or die (mysqli_error());
+    }
+    else{
+
+        $result = mysqli_query($koneksi, "DELETE FROM timeline WHERE idt = '$idt' LIMIT 1") or die (mysqli_error());
+
+    }
+
+}
+
+$count = mysqli_affected_rows($koneksi);
+
+ if ($count > 0) {
+     
+     $codex = "true";
+     echo json_encode($codex);
+ 
+ }
+ else{
+
+     $codex = "false";
+     echo json_encode($codex);
+     
+ }
+
+?>

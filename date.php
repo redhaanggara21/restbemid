b1b745ff8fc8c1dadcff7b4c2a4125c869e60629
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-02-24 14:30:23 +0700

    Update date.php

--- date.php
+++ date.php
@@ -1,10 +1,7 @@
 <?php
 
-//date_default_timezone_set("Asia/Jakarta")
-//$tanggal = date("Y-m-d H:i:s");
-//echo $tanggal;
-
 date_default_timezone_set("Asia/Bangkok");
-echo date_default_timezone_get();
+$tanggal = date("Y-m-d H:i:s");
+echo "date: ".$tanggal;
 
 ?>

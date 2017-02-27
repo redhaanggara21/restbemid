c1d50dc60bd60c85d3603e0d42055026b9d8cb61
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-02-27 22:25:08 +0700

    Update test_uploaddd.php

--- test_uploaddd.php
+++ test_uploaddd.php
@@ -7,14 +7,18 @@ header("Access-Control-Allow-Origin: *");
 include 'koneksi.php';
 include 'date.php';
 
-$target_path = "image_timeline/";
+//$target_path = "image_timeline/";
+
+$tmpFilePathP = $_FILES['file']['tmp_name'];
+$nameFilePathP = uniqid().str_replace(" ","_", basename($_FILES['file']['name']) );
+$filePathP = "image_timeline/" .$nameFilePathP;
  
-$target_path = $target_path . basename( $_FILES['file']['name']);
-$nama =  $_FILES['file']['name'];
+//$target_path = $target_path . basename( $_FILES['file']['name']);
+//$nama =  $_FILES['file']['name'];
 
-if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+if(move_uploaded_file($tmpFilePathP, $filePathP)) {
 
-    $sql= mysqli_query($koneksi, "INSERT INTO timeline (idunib,agenda,tagin,caption,tanggal,waktu,lat,lng,image) VALUES('xx','Meeting', 'false','masukbos','blmfix','$localDate','xxx','xxx','$nama') ") or die(mysqli_error());
+    $sql= mysqli_query($koneksi, "INSERT INTO timeline (idunib,agenda,tagin,caption,tanggal,waktu,lat,lng,image) VALUES('xx','Meeting', 'false','masukbos','blmfix','$localDate','xxx','xxx','$nameFilePathP') ") or die(mysqli_error());
 
     echo "Upload and move success";
 }

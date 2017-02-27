6eb9cd8b021e8a3af34bc0641459da569f3c1640
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-02-27 22:36:42 +0700

    Update test_uploaddd.php

--- test_uploaddd.php
+++ test_uploaddd.php
@@ -1,29 +1,28 @@
 <?php
 
-
 header("Access-Control-Allow-Origin: *");
-
 
 include 'koneksi.php';
 include 'date.php';
-
-//$target_path = "image_timeline/";
 
 $tmpFilePathP = $_FILES['file']['tmp_name'];
 $nameFilePathP = uniqid().str_replace(" ","_", basename($_FILES['file']['name']) );
 $filePathP = "image_timeline/" .$nameFilePathP;
  
-//$target_path = $target_path . basename( $_FILES['file']['name']);
-//$nama =  $_FILES['file']['name'];
 
 if(move_uploaded_file($tmpFilePathP, $filePathP)) {
 
     $sql= mysqli_query($koneksi, "INSERT INTO timeline (idunib,agenda,tagin,caption,tanggal,waktu,lat,lng,image) VALUES('xx','Meeting', 'false','masukbos','blmfix','$localDate','xxx','xxx','$nameFilePathP') ") or die(mysqli_error());
 
-    echo "Upload and move success";
+    $codex = "".$nameFilePathP;
+ 	  echo json_encode($codex);
 }
+
  else{
-    echo "There was an error uploading the file, please try again!";
+  
+     $codex = "false";
+ 	   echo json_encode($codex);
+  
 }
 
 ?>

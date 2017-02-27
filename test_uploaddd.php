a2c8f670031593a191fb3f90df9604c818407473
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-02-27 22:39:10 +0700

    Update test_uploaddd.php

--- test_uploaddd.php
+++ test_uploaddd.php
@@ -8,21 +8,22 @@ include 'date.php';
 $tmpFilePathP = $_FILES['file']['tmp_name'];
 $nameFilePathP = uniqid().str_replace(" ","_", basename($_FILES['file']['name']) );
 $filePathP = "image_timeline/" .$nameFilePathP;
+$codex="";
  
 
 if(move_uploaded_file($tmpFilePathP, $filePathP)) {
 
     $sql= mysqli_query($koneksi, "INSERT INTO timeline (idunib,agenda,tagin,caption,tanggal,waktu,lat,lng,image) VALUES('xx','Meeting', 'false','masukbos','blmfix','$localDate','xxx','xxx','$nameFilePathP') ") or die(mysqli_error());
 
-    $codex = "".$nameFilePathP;
- 	  echo json_encode($codex);
+    $codex = $nameFilePathP;
 }
 
  else{
-  
+
      $codex = "false";
- 	   echo json_encode($codex);
-  
+ 	   
 }
+
+echo json_encode($codex);
 
 ?>

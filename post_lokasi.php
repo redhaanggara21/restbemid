7dccd4fb199b3a8cb95a6bf66476e0985cbb9b29
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-03-03 22:23:02 +0700

    Create post_lokasi.php

---
+++ post_lokasi.php
@@ -0,0 +1,47 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include 'koneksi.php';
+include 'date.php';
+
+
+$nib = $_POST['nib'];
+
+$result = mysqli_query($koneksi,"SELECT COUNT(*) FROM lokasi WHERE niblo = '$nib' ") or die(mysqli_error());
+
+	$count = $result->fetch_row();
+	
+	if ($count > 0) {
+
+		$codex = $count;
+ 		echo json_encode($codex);
+
+	}
+	else{
+
+		$codex = $count;
+ 	 	echo json_encode($codex);
+
+	}
+
+?>

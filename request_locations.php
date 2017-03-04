404577e4ffbfd71f30ab2d164830c59084e4d559
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-03-04 20:10:15 +0700

    Create request_locations.php

---
+++ request_locations.php
@@ -0,0 +1,46 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include 'koneksi.php';
+
+$result = mysqli_query($koneksi,"SELECT * FROM lokasi ") or die(mysqli_error());
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+
+    $outp .= '{"niblo":"'                       . $rs["niblo"]                        . '",';
+
+    $outp .= '"lat":"'                       . $rs["lat"]                        . '",';
+    
+    $outp .= '"lng":"'                       . $rs["lng"]                        . '",';
+
+    $outp .= '"datetime":"'                       . $rs["datetime"]                       . '"}';
+
+
+}
+$outp ='{"records":['.$outp.']}';
+
+echo($outp);
+
+?>

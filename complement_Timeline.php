24c46029759be5b01a16be9abf5c776e847f1f12
redhaanggara <redhabayuanggara@yahoo.co.id>
2017-03-07 13:41:26 +0700

    Create complement_Timeline.php

---
+++ complement_Timeline.php
@@ -0,0 +1,48 @@
+<?php
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include 'koneksi.php';
+
+$result = mysqli_query($koneksi, "SELECT timeline.*, checkin.* FROM timeline,checkin WHERE timeline.idt = checkin.idt ") or die(mysqli_error());
+
+$outp = "";
+
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+
+    if ($outp != "") {$outp .= ",";}
+
+    $outp .= '{"idt":"'                       . $rs["idt"]                        . '",';
+
+    $outp .= '"nib":"'                       . $rs["nibin"]                        . '",';
+
+    $outp .= '"waktu":"'                       . $rs["waktu"]                        . '",';
+
+    $outp .= '"type":"'                       . $rs["type"]                       . '"}';
+
+
+}
+$outp ='{"records":['.$outp.']}';
+
+echo($outp);
+
+?>
